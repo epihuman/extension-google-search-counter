a414d4116be364ef2a9ff55517732677e29c6b34
epihuman <42874824+epihuman@users.noreply.github.com>
2018-08-31 20:53:24 +0200

    Second Save of content.js
    Content.js required by Chrome Extension

--- content.js
+++ content.js
@@ -1,7 +1,11 @@
+// We are creating counter and adding it to the body of the webpage
+
 document.body.style["counter-reset"] = 'gCounter';
 
-let gClass = document.getElementsByClassName('g');
+// We are styling .g classes to position relative
 
-document.styleSheets[0].insertRule('.g {position: relative; }',0);
+document.styleSheets[0].insertRule('.g {position: relative;}', 0);
 
-document.styleSheets[0].insertRule('.g:before {position: absolute; left: -3rem; top: -1rem; border: 1px solid red; border-radius: 50%; padding: 6px; background-color: orange;  content: counter(gCounter); counter-increment: gCounter;}', 0);
+// Finally we are adding OUR COUNTER to the webpage before all .g elements
+
+document.styleSheets[0].insertRule('.g:before {position: absolute; left: -3rem; top: -1rem; border: 1px solid red; border-radius: 50%; padding: 6px; background-color: orange content: counter(gCounter); counter-increment: gCounter}', 2);
